d1fde68f64d9f2b37b83850fc1bdd9d68a9f69c9
Jackson Ricardo <jackson2016.ricardo@gmail.com>
2023-05-26 11:12:47 -0300

    Cadastrando usuários no formulário

--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -4,7 +4,7 @@ import CampoTexto from '../CampoTexto';
 import ListaSuspensa from '../ListaSuspensa';
 import './Formulario.css';
 
-const Formulario = () => {
+const Formulario = (props) => {
     const times =[
         'Programação',
         'Redes de Computadores', 
@@ -22,7 +22,12 @@ const Formulario = () => {
 
     const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log('O formulário foi submetido.  =>', nome, cargo, imagem, time)
+        props.aoColaboradorCadastrado({
+            nome,
+            cargo,
+            imagem,
+            time
+        })
     }
 
     return (

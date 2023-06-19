82aa367bca7825961a75ad467af29a3618ab7eab
Jackson Ricardo <42697964+JacksonRicardo@users.noreply.github.com>
2023-06-19 16:54:46 -0300

    Iniciando desenvolvimento do card estático

---
+++ src/Colaborador/index.js
@@ -0,0 +1,16 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+
+    return(<div>
+        <div>
+            <img src='https://github.com/JacksonRicardo.png' alt='Jackson Ricardo'/>
+        </div>
+
+        <div>
+            <h4>Jackson Ricardo</h4>
+            <h5>Estagiário de Desenvolvimento</h5>
+        </div>
+    </div>)
+}
+export default Colaborador
